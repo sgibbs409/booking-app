3f16e4ca6489734ef27f7c49f48f2536a96a0ef2
sgibbs409 <asdfasd@asdfad.com>
2022-02-17 11:43:26 -0800

    For loop

--- main.go
+++ main.go
@@ -14,34 +14,30 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
 
-	// ask user for their name
-	fmt.Println("Enter your first name")
-	fmt.Scan(&firstName)
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Println("Enter your last name")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your first name")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your email address")
-	fmt.Scan(&email)
+		fmt.Println("Enter your last name")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter number of tickets")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter your email address")
+		fmt.Scan(&email)
 
-	remainingTickets = remainingTickets - userTickets
-	// bookings[0 ] = firstName + " " + lastName
-	bookings = append(bookings, firstName+" "+lastName)
+		fmt.Println("Enter number of tickets")
+		fmt.Scan(&userTickets)
 
-	// fmt.Printf("The whole array: %v\n", bookings)
-	// fmt.Printf("The first value: %v\n", bookings[0])
-	// fmt.Printf("Array type: %T\n", bookings)
-	// fmt.Printf("Array length: %v\n", len(bookings))
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets.  You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-	fmt.Printf("All our bookings: %v\n", bookings)
+		fmt.Printf("Thank you %v %v for booking %v tickets.  You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		fmt.Printf("All our bookings: %v\n", bookings)
+	}
 }
